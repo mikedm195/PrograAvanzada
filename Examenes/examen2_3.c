e83b4cf6703e326d5fb36c358ebcd6b4724d0025
root <root@localhost.localdomain>
2016-03-17 11:00:07 -0600

    Examen rápido 2-3. Concurrencia

--- Examenes/examen2_3.c
+++ Examenes/examen2_3.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <semaphore.h>
+#define N 150
 
-sem_t sem;
+sem_t semGen;
+sem_t semEmp;
 
 int main(){
+ 
+    pthread_t hilo1;
     
+    sem_init(&semGen,0,5);
+    sem_init(&semEmp,0,3);
+
+    pthread_t* threads = (pthread_t*) malloc(N * sizeof(pthread_t));
+
+
+    sem_destroy(&semGen);
+    sem_destroy(&semEmp);
+    free(threads);
+
     return 0;
 }
