0efb2b67f51bf9b3df678ecc5006e4bcaf346609
Mike <mike@localhost.localdomain>
2016-04-07 17:38:39 -0500

    Tarea 4. Concurrencia

--- Tarea4/ejercicio1.c
+++ Tarea4/ejercicio1.c
@@ -11,16 +11,23 @@ sem_t sillas;
 pthread_mutex_t blanca;
 int hayEnanitos;
 int acabo = 0;
+int comida = 0;
 
 void atiendeEnanito(){
-    if(hayEnanitos == 0){
+    while(hayEnanitos == 0){
+        pthread_mutex_lock(&blanca);
         printf("Blancanieves se fue porque no hay enanitos pendientes por servir\n");
-        while(hayEnanitos==0)
-            if(acabo == 1) pthread_exit(NULL);
+        sleep(5);
+        if(acabo == 1) break;
         printf("Llego Blancanieves\n");
+        pthread_mutex_unlock(&blanca);
     }
-    printf("Ya esta servido, puedes empezar a comer\n");
-    hayEnanitos--;
+    while(hayEnanitos > 0){
+        printf("Ya esta servido, puedes empezar a comer\n");
+        comida++;
+        sleep(1);
+        hayEnanitos--;
+    }
 }
 
 void blancan(){
@@ -35,9 +42,17 @@ void enano(void * p){
     printf("Ya me sente (enano %d)\n",id);
     hayEnanitos++;
     
+    pthread_mutex_lock(&blanca);
+    
+    while(comida==0);
+    printf("Voy a empezar a comer (enano %d)\n",id);
+    comida --;
+
+    pthread_mutex_unlock(&blanca);
+    
     sleep(rand()%10);
 
-    printf("Ya acabe de comer\n");
+    printf("Ya acabe de comer (enano %d)\n",id);
 
     sem_post(&sillas);
 
@@ -64,12 +79,10 @@ int main(){
     }
 
     for(aux=Enanito;aux<(Enanito+numEnanitos);aux++){
-        printf("Destruyendo Enano %d\n",aux-Enanito);
         pthread_join(*aux,NULL);
     }
     acabo = 1;
     pthread_join(Blancanieves,NULL);
-    printf("Destruyendo a Blancanieves\n");
     free(Enanito);
     return 0;
 }
--- Tarea4/ejercicio3.c
+++ Tarea4/ejercicio3.c
@@ -2,9 +2,79 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
-#include <sys/sem.h>
+#include <time.h>
+
+#define S 10
+#define R 5
+#define pesoRobot 6
+
+typedef struct Seccion {
+    pthread_mutex_t libre;
+    int peso;
+    int pesoActual;
+}seccion;
+
+seccion * secciones;
+
+void comprar(void * p){
+    int id = (int)p;
+    int i = 0;
+    while(i<S){
+        pthread_mutex_lock(&((secciones+i)->libre));
+        if(((secciones+i)->pesoActual + pesoRobot) <= (secciones+i)->peso ){
+            printf("Robot %d entra a la seccion %d\tBuscando productos...\n",id,i);
+            (secciones+i)->pesoActual += pesoRobot;
+            if(i>0){
+                pthread_mutex_lock(&((secciones+i-1)->libre));
+                (secciones+i-1)->pesoActual -= pesoRobot;
+                pthread_mutex_unlock(&((secciones+i-1)->libre));
+            }
+            int j=0;
+            //for(;j<S;j++)
+            //    printf("%d ",(secciones+j)->pesoActual);
+            //printf("\n");
+            pthread_mutex_unlock(&((secciones+i)->libre));
+            sleep(rand()%5);
+            printf("Robot %d acabo de rocojer los productos de la seccion %d\n",id,i);
+            i++;
+        }else{
+            printf("Robot %d no puede avanzar\n",id);
+            pthread_mutex_unlock(&((secciones+i)->libre));
+            sleep(5);
+        }
+    }
+    pthread_mutex_lock(&((secciones+i-1)->libre));
+    (secciones+S-1)->pesoActual -= pesoRobot;
+    pthread_mutex_unlock(&((secciones+i-1)->libre));
+
+    printf("Robot %d acabo\n",id);
+}
 
 int main(){
+    srand(time(NULL));
 
+    secciones = (seccion*)malloc(S*sizeof(seccion));
+    pthread_t * robots = (pthread_t*)malloc(R*sizeof(pthread_t));
+    pthread_t * aux;
+
+    int i = 0;
+    printf("Peso de secciones: ");
+    for(;i<S;i++){
+        (secciones+i)->peso = (rand()%10)+10;
+        (secciones+i)->pesoActual=0;
+        printf("%d ",(secciones+i)->peso);
+    }
+
+    printf("\n");
+    for(aux = robots;aux<(robots+R);aux++){
+        pthread_create(aux,NULL,comprar,(void*)(aux-robots));
+    }
+
+    for(aux = robots;aux<(robots+R);aux++){
+        pthread_join(*aux,NULL);
+    }
+
+    free(robots);
+    free(secciones);
     return 0;
 }
