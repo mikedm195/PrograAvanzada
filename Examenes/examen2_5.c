3289d7fc81c19544198b430f6ed8fa25de01324c
Miguel Del Moral <mikedm195@gmail.com>
2016-04-20 11:59:06 -0500

    Examen rápido 2-5. OpenMP

--- Examenes/examen2_5.c
+++ Examenes/examen2_5.c
@@ -1,6 +1,46 @@
-#include <omp.h>
 #include <stdio.h>
+#include <omp.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main(){
+#define N 6
+
+int main()
+{
+    srand(time(NULL));
+    int * num = (int*)malloc(N*sizeof(int));
+    int sum = 0;
+	int j = 0;
+	#pragma omp parallel shared(sum) private(j)
+	{
+    	#pragma omp sections
+        {
+            #pragma omp section
+            {
+                #pragma omp parallel for
+                for (j=0; j < N; j++){
+                    *(num+j) = rand() % 10;
+                    printf("num %d-->%d\n", j,*(num+j));
+                    sum += *(num+j);
+                }
+            }
+			#pragma omp section
+            {
+				int i = 0;
+            	omp_set_nested(1);
+                #pragma omp parallel num_threads(4) shared(i)
+                {
+                    #pragma omp critical
+                    {
+                        printf("thread--->%d\n", i);
+                        i++;
+                    }
+                }
+            }
+        }
+    }
+
+    printf("SUM :: %d\n", sum);
+
     return 0;
 }
