34352887ed00e1dd00d73b59fb5588b0cd7ca0b4
Miguel Del Moral <mikedm195@gmail.com>
2016-05-12 10:40:55 -0500

    Examen final

--- Examenes/final.c
+++ Examenes/final.c
@@ -5,12 +5,14 @@
 #include <semaphore.h>
 #include <time.h>
 #include <pthread.h>
+#include <signal.h>
 
 #define N 16
 #define S 4
 
 int mapa[N][N];
 int maxs[N/S];
+
 void creaMapa(){
 	int i=0,j=0;
 	int salida = N/4;
@@ -20,30 +22,34 @@ void creaMapa(){
 				mapa[i][j]=0;
 			else
 				mapa[i][j]=1;
-	for(i=salida-1;i<N;i+=salida)
-		if(mapa[i][i]==1)
-			mapa[i][i]=0;
+	for(i=0;i<N;i+=salida){
+		mapa[i][i]==8;
+		mapa[i+salida-1][i+salida-1]==8;
+	}
 	for(i=0;i<N/S;i++)
 		maxs[i]=N*N;
 }
 
+void dibuja(){
+	
+}
+
 void print(){
+	int rutaMin = 0;
 	int i=0,j=0,k=0;
-	int salida = N/4;
-	for(;i<N;i++){
+	for(i=0;i<N/S;i++)
+		rutaMin+=maxs[i];
+	for(i=0;i<N;i++){
 		for(j=0;j<N;j++){
 			printf("%d",mapa[i][j]);
-			if(j%salida==S-1)
-				printf("|");
 		}
 		printf("\n");
-		if(i%salida==S-1){
-			for(k=0;k<N+S;k++)
-				printf("_");
-			printf("\n");
-		}
 	}
-			
+
+	if(rutaMin > N*N)
+		printf("No se puede\n");
+	else	
+		printf("Ruta minima = %d\n",rutaMin);
 }
 
 void busca(void * p){
@@ -60,19 +66,33 @@ void buscar(int ini,int x, int y,int pasos){
 		if(pasos < maxs[ini/(N/S)])
 			maxs[ini/(N/S)] = pasos;
 	//if(pasos<max){
-		if(x < limite && y < limite && mapa[x+1][y+1]==0)
+		if(x < limite && y < limite && mapa[x+1][y+1] != 1){
+			if(maxs[ini/(N/S)]>=N*N)
+				mapa[x][y]=8;
 			buscar(ini,x+1,y+1,++pasos);
-		if(x < limite && mapa[x+1][y]==0)
+		}
+		if(x < limite && mapa[x+1][y] !=1){
+			if(maxs[ini/(N/S)]>=N*N)
+				mapa[x][y]=8;
 			buscar(ini,x+1,y,++pasos);
-		if(y < limite && mapa[x][y+1]==0)
+		}
+		if(y < limite && mapa[x][y+1] !=1){
+			if(maxs[ini/(N/S)]>=N*N)
+				mapa[x][y]=8;
 			buscar(ini,x,y+1,++pasos);
+		}
 	//}
+}
+
+void signalCtrl(int n){
+	print();
 }
 
 int main(){
 	srand(time(NULL));
 	creaMapa();
-	print();
+	
+	 signal(SIGUSR1, signalCtrl);
 
 	int numSec = N/S;
 
@@ -86,6 +106,8 @@ int main(){
 	aux = secciones;
 	for(aux=secciones;aux<(secciones+numSec);aux++)
 		pthread_join(*aux,NULL);
+	
+	print();
 	free(secciones);
 	return 0;
 }
